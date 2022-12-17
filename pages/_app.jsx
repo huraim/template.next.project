32fea62483b617fb80ef3e9addeda678f3da3757
taka <taka@mewpot.com>
2022-12-18 02:01:16 +0900

    [Docs] 초기세팅 (Scss, Env, GA, Directory)

---
+++ pages/_app.jsx
@@ -0,0 +1,11 @@
+import Layout from '../components/layout'
+import '../styles/globals.scss'
+
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
